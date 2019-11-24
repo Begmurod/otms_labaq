7e9f8332f91e9add90b5e393df5008ed6e122f4f
Bek <begmurod1999@mail.ru>
2019-11-24 15:37:41 +0500

    добавление отчета в word

--- Praktika3/Praktika3/Form1.Designer.cs
+++ Praktika3/Praktika3/Form1.Designer.cs
@@ -381,7 +381,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(10, 121);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(763, 220);
+            this.dataGridView1.Size = new System.Drawing.Size(809, 220);
             this.dataGridView1.TabIndex = 22;
             // 
             // tabPage3
